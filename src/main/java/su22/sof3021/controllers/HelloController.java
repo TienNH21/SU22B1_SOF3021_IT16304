f928915dc398b9d1820a67e33fb9f3dc89f4e49a
HoangTien339 <tiennh21@fpt.edu.vn>
2022-05-11 09:07:23 +0700

    1st commit

---
+++ src/main/java/su22/sof3021/controllers/HelloController.java
@@ -0,0 +1,15 @@
+package su22.sof3021.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("home")
+public class HelloController {
+	@RequestMapping(value="hello", method=RequestMethod.GET)
+	public String hello() {
+		return "hello";
+	}
+}
